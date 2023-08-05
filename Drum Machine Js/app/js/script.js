f61cca36f7bdf8a4cf8e5eb656c5744659e6a141
Peter Kwok <peter.kwok94@gmail.com>
2023-08-05 18:40:39 -0400

    added gain

--- Drum Machine Js/app/js/script.js
+++ Drum Machine Js/app/js/script.js
@@ -1,33 +1,50 @@
 // to do's
 
+// display
+const display = document.querySelector('#display')
 
 // audio context
-const audioContext = new AudioContext()
+const AudioContext = window.AudioContext || window.webkitAudioContext
+const audioCtx = new AudioContext()
 
 // audio sources
 const audioElems = document.querySelectorAll('.clip')
 const tracks = []
 for (let i = 0; i < audioElems.length; i++) {
-    tracks.push(audioContext.createMediaElementSource(audioElems[i]))
+    tracks.push(audioCtx.createMediaElementSource(audioElems[i]))
 }
 
 // gain
+const gain = audioCtx.createGain()
+const volume = document.querySelector('#volume')
+
+volume.addEventListener('input', () => {
+    gain.gain.value = volume.value
+
+    display.textContent = `Volume: ${Math.round(volume.value * 100)}%`
+})
 
 // destination
-const destination = audioContext.destination
+const destination = audioCtx.destination
 
 // connect
 for (let i = 0; i < tracks.length; i++) {
-    tracks[i].connect(destination)
+    tracks[i].connect(gain).connect(destination)
 }
 
-// pads
+// pads 
 const drumPads = document.querySelectorAll('.drum-pad')
 
 for (let i = 0; i < drumPads.length; i++) {
     drumPads[i].addEventListener('click', () => {
+        if (audioCtx.state === 'suspended') {
+            audioCtx.resume()
+        }
+
         audioElems[i].load()
         audioElems[i].play()
+
+        display.textContent = drumPads[i].dataset.description
     })
 }
 
@@ -35,11 +52,20 @@ const pads = document.querySelectorAll('.pad')
 for (let i = 0; i < drumPads.length; i++) {
     window.addEventListener('keydown', (e) => {
         if (e.which === pads[i].textContent.charCodeAt(0)) {
+            if (audioCtx.state === 'suspended') {
+                audioCtx.resume()
+            }
+
             audioElems[i].load()
             audioElems[i].play()
+
+            display.textContent = drumPads[i].dataset.description
         }
     })
 }
+
+
+
 
 
 
