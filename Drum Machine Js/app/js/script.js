fccbe8b2e3c863a01e36243fff4d627724b3ad8b
Peter Kwok <peter.kwok94@gmail.com>
2023-08-06 16:24:36 -0400

    finished kit swap

--- Drum Machine Js/app/js/script.js
+++ Drum Machine Js/app/js/script.js
@@ -14,6 +14,62 @@ for (let i = 0; i < audioElems.length; i++) {
     tracks.push(audioCtx.createMediaElementSource(audioElems[i]))
 }
 
+const kits = [
+    {
+        name: 'Kit 1',
+        sources: [
+            'audio/audio_set_1/blaster.mp3',
+            'audio/audio_set_1/click.mp3',
+            'audio/audio_set_1/point.mp3',
+            'audio/audio_set_1/start.mp3',
+            'audio/audio_set_1/synth.mp3',
+            'audio/audio_set_1/synth_2.mp3',
+            'audio/audio_set_1/beat_1.wav',
+            'audio/audio_set_1/beat_2.wav',
+            'audio/audio_set_1/beat_3.wav',
+        ]
+    },
+    {
+        name: 'Kit 2',
+        sources: [
+            'audio/audio_set_2/guitar_a.mp3',
+            'audio/audio_set_2/guitar_b.mp3',
+            'audio/audio_set_2/guitar_c.mp3',
+            'audio/audio_set_2/guitar_d.mp3',
+            'audio/audio_set_2/guitar_e.mp3',
+            'audio/audio_set_2/guitar_f.mp3',
+            'audio/audio_set_2/guitar_g.mp3',
+            'audio/audio_set_2/snare_drum.mp3',
+            'audio/audio_set_2/bass_drum.mp3'
+        ]
+    }
+]
+
+const kitSelect = document.querySelector('#kit_select')
+
+let currentKit = 0
+
+const controller = new AbortController()
+const signal = controller.signal
+
+function nextKit() {
+    controller.abort()
+
+    for (let i = 0; i < kits[currentKit].sources.length; i++) {
+        audioElems[i].setAttribute('src', kits[currentKit].sources[i])
+    }
+
+    if (currentKit < kits.length - 1) {
+        currentKit++
+    } else {
+        currentKit = 0
+    }
+
+    kitSelect.addEventListener('click', nextKit, signal)
+}
+
+nextKit(currentKit)
+
 // gain
 const gain = audioCtx.createGain()
 const volume = document.querySelector('#volume')
@@ -63,9 +119,3 @@ for (let i = 0; i < drumPads.length; i++) {
         }
     })
 }
-
-
-
-
-
-
